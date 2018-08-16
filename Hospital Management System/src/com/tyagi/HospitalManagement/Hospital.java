545e26b3131afea985401a7b79dcad2deb0e3f67
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-08-16 16:50:18 +0530

    Second Page !

--- Hospital Management System/src/com/tyagi/HospitalManagement/Hospital.java
+++ Hospital Management System/src/com/tyagi/HospitalManagement/Hospital.java
@@ -24,5 +24,16 @@ public class Hospital extends JApplet implements ActionListener {
 	JLabel lphone1 = new JLabel("Phone No.");
 	JLabel lbill1 = new JLabel("Bill");
 
+	JLabel lid2 = new JLabel("Enter Patient ID");
+	JLabel lname2 = new JLabel("Name");
+	JLabel lage2 = new JLabel("Age");
+	JLabel lsex2 = new JLabel("Sex");
+	JLabel lfhname2 = new JLabel("Father's/Husband's Name");
+	JLabel ldoa2 = new JLabel("Date of Admission");
+	JLabel ldod2 = new JLabel("Date of Discharge");
+	JLabel ladd2 = new JLabel("Address");
+	JLabel lphone2 = new JLabel("Phone No.");
+	JLabel lbill2 = new JLabel("Bill");
+
 	
 }
