4ae2716347a4bb97fc09ecaa4f1d6a48c1d974f9
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-08-16 16:49:18 +0530

    Front Page !

---
+++ Hospital Management System/src/com/tyagi/HospitalManagement/Hospital.java
@@ -0,0 +1,28 @@
+package com.tyagi.HospitalManagement;
+
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import java.sql.*;
+import java.util.*;
+
+public class Hospital extends JApplet implements ActionListener {
+	JFrame f1 = new JFrame("TYAGI LIFELINE HOSPITALS");
+	JPanel p1 = new JPanel();
+	JPanel p2 = new JPanel();
+	JPanel p3 = new JPanel();
+	JTabbedPane tp = new JTabbedPane();
+
+	JLabel lid1 = new JLabel("Patient ID");
+	JLabel lname1 = new JLabel("Name");
+	JLabel lage1 = new JLabel("Age");
+	JLabel lsex1 = new JLabel("Sex");
+	JLabel lfhname1 = new JLabel("Father's/Husband's Name");
+	JLabel ldoa1 = new JLabel("Date of Admission");
+	JLabel ldod1 = new JLabel("Date of Discharge");
+	JLabel ladd1 = new JLabel("Address");
+	JLabel lphone1 = new JLabel("Phone No.");
+	JLabel lbill1 = new JLabel("Bill");
+
+	
+}
